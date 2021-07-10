fb234fc7df503bd5921cd24384b5467e4cbf724e
Vishok Shukla <vishok.bhumca19@gmail.com>
2021-07-10 22:13:49 +0530

    Level-3 Encryption - Hashing

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -25,8 +25,6 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
-
 const User = mongoose.model("User", userSchema);
 
 app.get("/", function(req, res) {
@@ -45,7 +43,7 @@ app.route("/login")
             console.log(err);
         } else {
             if(foundUser) {
-                if(foundUser.password === password) {
+                if(foundUser.password === md5(password)) {
                     res.render("secrets");
                 } 
             }
@@ -60,7 +58,7 @@ app.route("/register")
 .post(function(req, res) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function(err) {
         if(err) {
