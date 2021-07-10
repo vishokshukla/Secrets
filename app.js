77e06ec1a408c7860547fd4d7d670d9c33a71422
Vishok Shukla <vishok.bhumca19@gmail.com>
2021-07-10 23:38:34 +0530

    Level - 4 Encryption - Hashing(bcrypt)

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -43,9 +44,11 @@ app.route("/login")
             console.log(err);
         } else {
             if(foundUser) {
-                if(foundUser.password === md5(password)) {
-                    res.render("secrets");
-                } 
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if(result === true) {
+                        res.render("secrets");
+                    }
+                });
             }
         }
     });
@@ -56,17 +59,19 @@ app.route("/register")
     res.render("register");
 })
 .post(function(req, res) {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save(function(err) {
+            if(err) {
+                console.log(err);
+            } else {
+                res.render("secrets")
+            }
+        });
     });
-    newUser.save(function(err) {
-        if(err) {
-            console.log(err);
-        } else {
-            res.render("secrets")
-        }
-    })
 })
 
 app.listen(3000, function() {
